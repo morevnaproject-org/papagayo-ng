12a54d03a22bdd448a1eb2fa5e18a832d0054a48
benjamin.a.lau <benjamin.a.lau@edb7da35-b3c1-9830-0d95-cc27b01f0ebc>
2010-08-28 04:46:41 +0000

    there was a change to the wx api that broke this... which is why setting the FPS is broken in the current version of papagayo
    git-svn-id: https://papagayo.googlecode.com/svn/trunk@3 edb7da35-b3c1-9830-0d95-cc27b01f0ebc

--- LipsyncFrame.py
+++ LipsyncFrame.py
@@ -58,7 +58,7 @@ class DigitOnlyValidator(wx.PyValidator):
 		return True
 
 	def OnChar(self, event):
-		key = event.KeyCode()
+		key = event.GetKeyCode()
 
 		if key < wx.WXK_SPACE or key == wx.WXK_DELETE or key > 255:
 			event.Skip()
