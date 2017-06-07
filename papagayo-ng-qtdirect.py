b4c0480d5adf94ee59fc13ccc0db9e026b544296
Stefan Murawski <steveway1@googlemail.com>
2017-06-07 12:54:45 +0200

    Use the .ui File directly instead of pre-converting it. UI Icons currently are compiled from qt-icons.qrc using pyside-rcc to papagayongrcc.py which can then be used by a simple import.

---
+++ papagayo-ng-qtdirect.py
@@ -0,0 +1,19 @@
+from PySide import QtCore, QtGui, QtUiTools
+import papagayongrcc
+
+
+def loadUiWidget(uifilename, parent=None):
+    loader = QtUiTools.QUiLoader()
+    uifile = QtCore.QFile(uifilename)
+    uifile.open(QtCore.QFile.ReadOnly)
+    ui = loader.load(uifile, parent)
+    uifile.close()
+    return ui
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = loadUiWidget("./rsrc/papagayo-ng.ui")
+    MainWindow.show()
+    sys.exit(app.exec_())
