bd3d990489ce0ed6a1a4c752082fd35859f0f785
egoZentric <egoZentric@gmail.com>
2018-09-01 20:41:46 +0200

    Tracking down wxGlade slime
    wxglade code generation was set to use the gettext module - BUT NOWHER in the project is any I18N or L10N used (yet.)

--- papagayo-ng.py
+++ papagayo-ng.py
@@ -24,7 +24,26 @@ except ImportError:
 import wx
 import os
 import sys
+
+# ### TODO : feature request : Add international support.
+
+# UNUSED MODULE
 import gettext
+##    Internationalization and localization support.
+##
+##    https://docs.python.org/3/library/gettext.html?highlight=gettext#module-gettext
+##
+##    This module provides internationalization (I18N) and localization (L10N)
+##    support for a Python program by providing an interface to the GNU gettext
+##    message catalog library. I18N refers to the operation by which a program is
+##    made aware of multiple   languages.  L10N refers to the adaptation of your
+##    program, once internationalized, to the local language and cultural habits.
+##
+##    [As far as i can tell, This is automatically inserted by wxGlade code generator
+##    and is not used anywhere in this projecy (yet) ]\eZ
+# ### TODO : strip _() message functions - this is a job for grep x-d:-)
+
+
 from LipsyncFrame import LipsyncFrame
 
 
@@ -42,11 +61,17 @@ class LipsyncApp(wx.App):
 
 # end of class LipsyncApp
 
+
 if __name__ == "__main__":
+
+    # UNUSED module gettext
+    # Setup for GNU (I18N) and (L10N)
     try:
         gettext.install("papagayo-ng", unicode=True)
     except TypeError:
         gettext.install("papagayo-ng")
+    # END OF UNUSED BLOCK
+
     papagayo = LipsyncApp(0)
     papagayo.mainFrame.TheApp = papagayo
     papagayo.mainFrame.waveformView.TheApp = papagayo
